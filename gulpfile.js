23ff222f9440d019c6ecf2be89c1091cb4c5d6c5
yat8823jp <yat@yat-net.com>
2015-08-05 10:09:53 +0900

    first-commit

---
+++ gulpfile.js
@@ -0,0 +1,149 @@
+/*
+ * Global variables
+ */
+var gulp = require( 'gulp' ),
+	scss = require( 'gulp-ruby-sass' ),
+	browserSync = require( 'browser-sync' ),
+	plumber = require( 'gulp-plumber' ),//エラー通知
+	notify = require( 'gulp-notify' ),//エラー通知
+	imagemin = require( 'gulp-imagemin' ),//画像圧縮
+	imageminPngquant = require( 'imagemin-pngquant' ),//png画像の圧縮
+	pleeease = require( 'gulp-pleeease' ),//ベンダープレフィックス
+	useref = require('gulp-useref'),//ファイル結合
+	gulpif = require('gulp-if'),// if文
+	uglify = require('gulp-uglify'),//js圧縮
+	minifyCss = require('gulp-minify-css'),//css圧縮
+	del = require('del'),//ディレクトリ削除
+	runSequence = require('run-sequence'),//並行処理
+	fs = require('fs'),//jsonファイル読み込み
+	ejs = require("gulp-ejs"),
+	replace = require('gulp-replace'),
+	paths = {
+		rootDir : 'dev',
+		dstrootDir : 'htdocs/',
+		srcDir : 'dev/images',
+		dstDir : 'htdocs/images',
+		serverDir : 'ms_test'
+	}
+
+
+
+/*
+ * Compass
+ */
+gulp.task( 'scss', function() {
+	return scss( 'dev/scss/', { style: 'expanded' } )
+		.pipe(plumber({
+			errorHandler: notify.onError( 'Error: <%= error.message %>' )
+		}))
+		.pipe( gulp.dest( 'dev/css' ) );
+});
+
+/*
+ * Pleeease
+ */
+gulp.task('pleeease', function() {
+	return gulp.src( 'dev/css/*.css' )
+		.pipe( pleeease({
+			sass: true
+		}) )
+		.pipe( gulp.dest('dev/css/') );
+});
+
+/*
+ * Imagemin
+ */
+gulp.task( 'imagemin', function(){
+	var srcGlob = paths.srcDir + '/**/*.+(jpg|jpeg|gif|svg)';
+	var dstGlob = paths.dstDir;
+	var imageminOptions = {
+		optimizationLevel: 7
+	};
+
+	gulp.src( srcGlob )
+		.pipe( imagemin( imageminOptions ) )
+		.pipe( gulp.dest( paths.dstDir ) );
+});
+gulp.task('imageminPngquant', function () {
+	gulp.src( paths.srcDir + '/**/*.png' )
+		.pipe( imageminPngquant( {quality: '65-80', speed: 1 } )())
+		.pipe( gulp.dest( paths.dstDir ) );
+});
+
+/*
+ * Useref
+ */
+gulp.task('html', function () {
+	var assets = useref.assets();
+
+	return gulp.src( 'dev/**/*.+(html|php)' )
+		.pipe( gulpif( '*.html', replace( '/images', '/' + paths.serverDir + '/images' ) ) )
+		.pipe( gulpif( '*.html', replace( 'href="/', 'href="/' + paths.serverDir + '/' ) ) )
+		.pipe( assets )
+		// .pipe( gulpif( '*.js', uglify() ) )
+		// .pipe( gulpif( '*.css', minifyCss() ) )
+		.pipe( assets.restore() )
+		.pipe( useref() )
+		.pipe( gulp.dest( 'htdocs' ) );
+});
+
+/*
+* ejs
+*/
+gulp.task('ejs', function () {
+	gulp.src(["dev/**/*.ejs", '!' + "dev/**/_*.ejs"])
+		.pipe(ejs())
+		.pipe( gulp.dest( 'dev' ) );
+});
+
+
+/*
+ * Browser-sync
+ */
+gulp.task( 'browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'dev',
+			routes: {
+				"/bower_components": "bower_components"
+			}
+		},
+		// proxy: "localhost:8888",
+		notify: true
+	});
+});
+gulp.task( 'bs-reload', function () {
+	browserSync.reload();
+});
+
+/*
+ * Default
+ */
+gulp.task( 'default', ['browser-sync'], function() {
+	var bsList = [
+		'dev/**/*.html',
+		'dev/js/**/*.js',
+		'dev/css/*.css',
+		'dev/css/*.php'
+	];
+	gulp.watch( 'dev/scss/**/*.scss', ['scss'] );
+	gulp.watch( 'dev/css/*.css', ['pleeease'] );
+	gulp.watch( bsList, ['bs-reload']);
+});
+
+/*
+ * Build
+ */
+gulp.task( 'clean', del.bind( null, ['htdocs'] ) );
+gulp.task( 'devcopy', function () {
+	return gulp.src([
+		'dev/*.*',
+		'!dev/**/*.ejs',
+		'!dev/*.html'
+	], {
+		dot: true
+	}).pipe( gulp.dest( 'htdocs' ) );
+});
+gulp.task( 'build', ['clean'], function ( cb ) {
+	runSequence( 'scss', 'ejs', ['html', 'imagemin', 'imageminPngquant', 'devcopy'], cb );
+});
---
+++ readme.md
@@ -0,0 +1,45 @@
+#Gulp　開発環境構築
+
+##パッケージ一覧
+* gulp
+* gulp-ruby-sass
+* browserSync
+* plumber //エラー通知
+* notify //エラー通知
+* gulp-imagemin //画像圧縮
+* imagemin-pngquant //png画像の圧縮
+* gulp-pleeease //ベンダープレフィックス
+* gulp-useref //ファイル結合
+* gulp-if //if文
+* gulp-uglify //js圧縮
+* gulp-minify-css //css圧縮
+* del //ディレクトリ削除
+* run-sequence //並行処理
+* gulp-ejs //htmlの書き出し＆テンプレートファイルインクルード,
+* gulp-replace //文字列置き換え
+
+##依存アプリケーション
+
+* bower
+* npm
+* gulp
+
+### インストール
+
+#### npmインストール
+` npm install `
+
+エラーが出たら `sudo` をつける
+
+#### bower
+` bower install `
+
+エラーが出たら `sudo` をつける
+
+### 起動コマンド
+` gulp `　- defaultで設定しているtaskが起動する
+
+` gulp *** ` - *** のtaskを起動する
+
+
+
